5b03d763b0023bbfae1be3e8f3626a2120c390c5
shijie.gan <shijiegan.gs@gmail.com>
2024-07-26 02:49:16 +0800

    complete react tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const items = [
+    "Kuala Lumpur",
+    "Penang",
+    "Johor Bahru",
+    "Kuching",
+    "Kota Kinabalu",
+  ];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  const [alertVisible, setAlertVisible] = useState(false);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+
+      <Button color="success" onClick={() => setAlertVisible(true)}>
+        Click Me!
+      </Button>
+
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>
+          Click on a city to select it
+        </Alert>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const message = items.length === 0 && <p>There are no items in the list</p>;
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {message}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
